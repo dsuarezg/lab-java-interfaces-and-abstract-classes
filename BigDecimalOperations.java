efc6d806be3557d36bb292b5e467ca4eae0300eb
Daniel Suárez García <dsuarezg@uoc.edu>
2025-04-03 15:02:46 +0200

    BigDecimal Task

---
+++ BigDecimalOperations.java
@@ -0,0 +1,23 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class BigDecimalOperations {
+
+
+    public static double roundBigDecimal(BigDecimal bigDecimal){
+
+        return bigDecimal.setScale(2, RoundingMode.HALF_UP).doubleValue();
+    }
+
+    public static BigDecimal reverseBigDecimal(BigDecimal bigDecimal){
+
+        return bigDecimal.setScale(1, RoundingMode.HALF_UP).negate();
+    }
+
+    public static void main(String[] args) {
+        BigDecimal bigDecimal = new BigDecimal("4.2545");
+        System.out.println("Rounded Value: " +  roundBigDecimal(bigDecimal));
+        System.out.println("Reversed Value: " + reverseBigDecimal(bigDecimal));
+    }
+
+}
